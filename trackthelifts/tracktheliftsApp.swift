5ed4f1d2c12fe9fbf2a2bb66ee6eb8dedc4e8691
Cratis1831 <asotoudeh18@hotmail.com>
2025-08-21 12:26:44 -0400

    Initial Commit

---
+++ trackthelifts/tracktheliftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tracktheliftsApp.swift
+//  trackthelifts
+//
+//  Created by Ashkan Sotoudeh on 2025-08-21.
+//
+
+import SwiftUI
+
+@main
+struct tracktheliftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
